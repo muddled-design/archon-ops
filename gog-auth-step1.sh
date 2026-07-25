50f8e00fce0cd356975572a9896a39323e8b0a3b
muddled-design <muddleddev@gmail.com>
2026-07-25 14:31:28 -0700

    gog OAuth remote flow step 1

---
+++ gog-auth-step1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export HOME=/root
+gog auth add muddleddev@gmail.com --remote --step 1 --services gmail,calendar --gmail-scope readonly > /root/.archon-bridge/gog-step1.log 2>&1
+echo "written"
